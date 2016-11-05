5f06c597a0da7de696d2ea8ef8147f6c158e39a0
abdsh <abd.shobi@gmail.com>
2016-11-05 23:23:18 +0330

    final

--- pydownloader.py
+++ pydownloader.py
@@ -1,24 +1,28 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
+import urllib.request
+
 
 class Downloader(QDialog):
     def __init__(self):
         QDialog.__init__(self)
 
         layout = QVBoxLayout()
-        url = QLineEdit()
-        save_location = QLineEdit()
-        progress = QProgressBar()
+        self.url = QLineEdit()
+        self.save_location = QLineEdit()
+        self.progress = QProgressBar()
         download = QPushButton("Download")
+        browse = QPushButton("Browse")
 
-        url.setPlaceholderText('URL')
-        save_location.setPlaceholderText('File save location')
+        self.url.setPlaceholderText('URL')
+        self.save_location.setPlaceholderText('File save location')
 
-        progress.setValue()
-        layout.addWidget(url)
-        layout.addWidget(save_location)
-        layout.addWidget(progress)
+        self.progress.setValue(0)
+        layout.addWidget(self.url)
+        layout.addWidget(self.save_location)
+        layout.addWidget(browse)
+        layout.addWidget(self.progress)
         layout.addWidget(download)
 
 
@@ -26,6 +30,36 @@ class Downloader(QDialog):
 
         self.setWindowTitle('pyDownloader')
         self.setFocus() #set focus on entire app
+
+        download.clicked.connect(self.download)
+        browse.clicked.connect(self.browse_file)
+
+    def browse_file(self):
+        save_file = QFileDialog.getSaveFileName(self, caption="save File As",
+                                                directory=".", filter="All Files (*.*)")
+        self.save_location.setText(QDir.toNativeSeparators(save_file))
+
+    def download(self):
+        url = self.url.text()
+        save_location = self.save_location.text()
+
+        try:
+            urllib.request.urlretrieve(url, save_location, self.report)
+        except Exception:
+            QMessageBox.warning(self, "Warning", "The download failed")
+            return
+
+        QMessageBox.information(self, "Information", "The download is complete")
+        self.progress.setValue(0)
+        self.url.setText("")
+        self.save_location.setText("")
+
+    def report(self, blocknum, blocksize, totalsize):
+        readsofar = blocknum * blocksize
+        if totalsize > 0:
+            percent = readsofar * 100 / totalsize
+            self.progress.setValue(int(percent))
+
 
 
 
